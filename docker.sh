7fd143b6e861954999c67da27ccc3f80bd756400
amitsharma <55508548+amit17133129@users.noreply.github.com>
2021-03-11 22:40:54 +0530

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,1 +1,13 @@
-hello jenkins
+cat <<EOF > /etc/yum.repos.d/docker.repo
+[docker]
+name=docker
+baseurl=https://download.docker.com/linux/centos/7/x86_64/stable/
+gpgcheck=0
+EOF
+yum install docker-ce --nobest -y
+
+systemtcl start docker
+
+docker ps
+
+docker run -dit --name myos centos:7
