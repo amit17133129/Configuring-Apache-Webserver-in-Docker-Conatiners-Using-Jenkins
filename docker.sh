9d8471454896309d18cc897ab56eae34f05345bd
amitsharma <55508548+amit17133129@users.noreply.github.com>
2021-03-11 23:28:51 +0530

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -4,10 +4,10 @@ name=docker
 baseurl=https://download.docker.com/linux/centos/7/x86_64/stable/
 gpgcheck=0
 EOF
-yum install docker-ce --nobest -y
+sudo yum install docker-ce --nobest -y
 
-systemtcl start docker
+sudo systemtcl start docker
 
-docker ps
+sudo docker ps
 
-docker run -dit --name myos centos:7
+sudo docker run -dit --name myos centos:7
