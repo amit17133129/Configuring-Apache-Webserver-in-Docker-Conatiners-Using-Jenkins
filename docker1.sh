c823c72f50c766d691adcd19d636f1533f5156d9
amitsharma <55508548+amit17133129@users.noreply.github.com>
2021-03-12 13:46:09 +0530

    Update docker1.sh

--- docker1.sh
+++ docker1.sh
@@ -9,7 +9,10 @@ sudo yum install docker-ce --nobest -y
 sudo systemctl restart docker
 
 sudo docker ps
-
-sudo docker run -dit --name myos centos:7
-
-sudo docker ps
+docker rm -f $(docker ps -a)
+docker run -dit --name myos1 -p 8085:80 centos:7
+docker exec -it myos1  yum install httpd  -y
+docker exec -it myos1  yum install net-tools  -y
+docker cp    myos1:/root/a.html /var/www/html
+docker exec -it myos1   docker ps
+docker exec -it myos1  /usr/sbin/httpd/
