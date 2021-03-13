fe7330e2eb87364eebedc4d256805e1b569176bc
amitsharma <55508548+amit17133129@users.noreply.github.com>
2021-03-13 21:21:36 +0530

    Update docker1.sh

--- docker1.sh
+++ docker1.sh
@@ -14,4 +14,4 @@ sudo docker run -dit --name myos1 -p 8085:80 centos:7
 sudo docker exec -i myos1  yum install httpd  -y
 sudo docker exec -i myos1  yum install net-tools  -y
 sudo docker cp  /root/a.html  myos1:/var/www/html/
-sudo docker exec -i myos1  /usr/sbin/httpd/
+
